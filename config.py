1b20fdd9c2d7b8061828fc07d7b773cf3e27899c
Error <93877180+ProErrorXD@users.noreply.github.com>
2021-11-24 19:20:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 from heroku3 import from_key
 from pyrogram import Client
 
-API_ID = int(os.environ.get("API_ID", "6"))
-API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+API_ID = int(os.environ.get("API_ID", "1714588"))
+API_HASH = os.environ.get("API_HASH", "78c27bf90c81f15a8af4aa0aeeadfc42")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "2126972604:AAEyb_v4qJptARMzyyIxNhWR1UaALl_FR6w")
 ARQ_API_KEY = "XQYJAL-HTSZIK-YALWDS-TJPWMO-ARQ" 
 LANGUAGE = "hi"
 ARQ_API_BASE_URL = "https://thearq.tech"
